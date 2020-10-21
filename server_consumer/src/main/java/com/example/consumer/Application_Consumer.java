bbc31ef944bb125c6665b75562f36a0efce4199d
dlz <dlz0729@126.com>
2020-10-21 16:35:09 +0800

    修改

--- server_consumer/src/main/java/com/example/consumer/Application_Consumer.java
+++ server_consumer/src/main/java/com/example/consumer/Application_Consumer.java
@@ -1,7 +1,10 @@
 package com.example.consumer;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -21,7 +24,17 @@ public class Application_Consumer {
     }
 
     @Bean
+    @LoadBalanced //负载均衡注解
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public IRule myRule() {
+        //随机策略
+        return new RandomRule();
+        // return new RetryRule();
+        //return new WeightedResponseTimeRule();
+    }
+
 }
--- server_consumer/src/main/java/com/example/consumer/controller/ConsumerController.java
+++ server_consumer/src/main/java/com/example/consumer/controller/ConsumerController.java
@@ -1,13 +1,11 @@
 package com.example.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
 
 /**
  * @author dlz
@@ -27,14 +25,14 @@ public class ConsumerController {
     public String get() {
         //资源路径 写死的只能访问固定服务
         // String url = "http://127.0.0.1:8001/provider/get";
-
-        String url;
         System.out.println("请求进来了");
-        //获取eureka中注册的TEST-PROVIDER的实例
-        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("TEST-PROVIDER");
-        ServiceInstance serviceInstance = serviceInstances.get(0);
-        //动态获取服务拼接请求地址
-        url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/provider/get";
+        // String url;
+        // //获取eureka中注册的TEST-PROVIDER的实例
+        // List<ServiceInstance> serviceInstances = discoveryClient.getInstances("TEST-PROVIDER");
+        // ServiceInstance serviceInstance = serviceInstances.get(0);
+        // //动态获取服务拼接请求地址
+        // url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/provider/get";
+        String url="http://TEST-PROVIDER/provider/get";
         return restTemplate.getForObject(url, String.class);
     }
 }
