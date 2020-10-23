01e2f0f0be8540b459cc979d14a10c053f041103
dlz <dlz0729@126.com>
2020-10-23 16:14:59 +0800

    使用配置中心

--- server_provider/src/main/java/com/example/provider/controller/UserController.java
+++ server_provider/src/main/java/com/example/provider/controller/UserController.java
@@ -4,12 +4,11 @@ import com.example.provider.entity.User;
 import com.example.provider.service.UserService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,12 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 @MapperScan(basePackages = {"com.example.provider.mapper"})
+@RefreshScope //及时刷新配置，哪个类需要加载配置加哪个类
 public class UserController {
     @Autowired
     private UserService userService;
 
-    String msg;
+    @Value("${test.name}")
+    private String name;
 
+    String msg;
+    
     @RequestMapping("/add")
     public String addUser() {
         User u = new User(null, "张三丰", "123");
@@ -38,13 +41,15 @@ public class UserController {
             msg = "add失败";
         }
         return msg;
-    }
-
+    }   
+    
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
     public String getUser(@PathVariable Integer id) {
         User user = userService.get(id);
         if (!ObjectUtils.isEmpty(user)) {
-            msg = "id:" + id + " " + "lohinName:" + user.getLoginName() + " " + "passWord:" + user.getPassWord();
+            msg = "id:" + id + " " + "lohinName:" + user.getLoginName() + " " + "passWord:" + user.getPassWord()
+                    + "\n"
+                    + "配置文件中的test.name:" + name;
         } else {
             msg = "没有查询到id为" + id + "的用户";
         }
